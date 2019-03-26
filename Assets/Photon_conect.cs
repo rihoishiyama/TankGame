e4cc6b6b5e5b5bfb13e10ca2967a6b40ef0ff304
MarinaFurusato <akijas08@gmail.com>
2019-03-26 18:40:54 +0900

    add Photon

---
+++ Assets/Photon_conect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Photon_conect : MonoBehaviour
+{
+    [SerializeField]
+    private string m_resourcePath = "";
+    [SerializeField]
+    private float m_randomCircle = 4.0f;
+    private const string ROOM_NAME = "RoomA";
+    public bool AutoConnect = true;
+    public byte Version = 1;
+    private bool ConnectInUpdate = true;
+    // cube生成
+    public void SpawnObject()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(0, 0, 0), Quaternion.identity, 0);
+    }
+    // 生成するcubeの座標をランダム生成
+    private Vector3 GetRandomPosition()
+    {
+        var rand = Random.insideUnitCircle * m_randomCircle;
+        return rand;
+    }
+    public virtual void Update()
+    {
+        if (ConnectInUpdate && AutoConnect && !PhotonNetwork.connected)
+        {
+            Debug.Log("Update() was called by Unity. Scene is loaded. Let's connect to the Photon Master Server. Calling: PhotonNetwork.ConnectUsingSettings();");
+            ConnectInUpdate = false;
+            PhotonNetwork.ConnectUsingSettings(Version + "." + SceneManagerHelper.ActiveSceneBuildIndex);
+        }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    SpawnObject();
+        //}
+    }
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
+        //PhotonNetwork.JoinRandomRoom();
+        //PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
+        PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, new RoomOptions(), TypedLobby.Default);
+    }
+    public void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
+        SpawnObject();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float moveSpeed = 100f;
+    public Joystick joystick;
+
+    void Start()
+    {
+        joystick = GameObject.Find("Joystick").GetComponent<Joystick>();
+    }
+
+	void Update()
+	{
+		Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+		if (moveVector != Vector3.zero)
+		{
+			transform.rotation = Quaternion.LookRotation(moveVector);
+			transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+		}
+	}
+}
