b6ac0889701001648deb5fb093d10acf1eaf7efd
服部壮一郎 <clover_2468@yahoo.co.jp>
2019-03-31 15:02:12 +0900

    reboundlimit

--- Assets/Scripts/Shotbullet.cs
+++ Assets/Scripts/Shotbullet.cs
@@ -7,16 +7,14 @@ public class Shotbullet : MonoBehaviour {
     public GameObject shellPrefab;
     public float shotSpeed;
     public AudioClip shotSound;
-    public int  bulletcount;
-    public int  sreboundcount;
+    public static int  bulletcount;
 
  
     void Update () {
          
         // もしも「Fire1」というボタンが押されたら（条件）
         if(Input.GetButtonDown("Fire1")){
- 
-            // ①Shotという名前の関数（命令ブロック）を実行する。
+
             if(bulletcount < 5 ){
 
                 Shot();
@@ -28,29 +26,26 @@ public class Shotbullet : MonoBehaviour {
 
             else{
                 AudioSource.PlayClipAtPoint(shotSound, transform.position);
-            }
-
-
+            }           
         }
     }
-     
+ 
  
     public void Shot(){
-        
-        
+         
         // プレファブから砲弾(Shell)オブジェクトを作成し、それをshellという名前の箱に入れる。
         GameObject shell =  (GameObject)Instantiate(shellPrefab, transform.position, Quaternion.identity);
-
+ 
         // Rigidbodyの情報を取得し、それをshellRigidbodyという名前の箱に入れる。
         Rigidbody shellRigidbody = shell.GetComponent<Rigidbody>();
-
+ 
         // shellRigidbodyにz軸方向の力を加える。
         shellRigidbody.AddForce(transform.forward * shotSpeed);
+ 
+    }
 
-            
-        }
-        
-            
-                
     
+
+
+
 }
