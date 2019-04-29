cf954c19b381d70938a78c150a5bd69ab575077d
市川竜也 <tatsunari101@gmail.com>
2019-04-29 16:11:48 +0900

    create OnFireButton in player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+	[SerializeField]
+	private Button onFireButton;
+	[SerializeField]
+	private Shotbullet shotBullet;
 	public float moveSpeed = 10f;
 	public Joystick joystick;
-	
+
 	void Start()
-    {
-        joystick = GameObject.Find("Joystick").GetComponent<Joystick>();
-    }
+	{
+		joystick = GameObject.Find("Joystick").GetComponent<Joystick>();
+		onFireButton = GameObject.Find("OnFireButton").GetComponent<Button>();
+		onFireButton.onClick.AddListener(() => shotBullet.ButtonShot());
+	}
 
 
 	void Update()
@@ -24,10 +31,10 @@ public class Player : MonoBehaviour
 
 	private void OnCollisionEnter(Collision other)
 	{
-		if(other.gameObject.CompareTag("Bullet"))
+		if (other.gameObject.CompareTag("Bullet"))
 		{
 			Destroy(this.gameObject);
 			Destroy(other.gameObject);
-		}	
+		}
 	}
 }
