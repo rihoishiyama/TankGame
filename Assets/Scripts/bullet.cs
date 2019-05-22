16e9972b0b4a8aa29528fb8ee1668c980f2aaf24
服部壮一郎 <clover_2468@yahoo.co.jp>
2019-05-22 19:58:56 +0900

    弾の反射の修正2

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -7,33 +7,45 @@ public class bullet : MonoBehaviour
 	public AudioClip reboundSound;
 	public int reboundcount;
 
+
+	void Start()
+	{
+		photonTransformView = GetComponent<PhotonTransformView>();
+		photonView = PhotonView.Get(this);
+	}
+
+
 	void OnCollisionEnter(Collision other)
 	{
-		if (other.gameObject.CompareTag("Wall")){
+		if (photonView.isMine)
+		{
+			if (other.gameObject.CompareTag("Wall")){
 
-			reboundcount += 1;
+				reboundcount += 1;
 
-			if (reboundcount > 1)
-			{
-				Shotbullet.bulletcount -= 1;
+				if (reboundcount > 1)
+				{	
+					Shotbullet.bulletcount -= 1;
+
+					Destroy(this.gameObject);
+				}
+				else
+				{
+					AudioSource.PlayClipAtPoint(reboundSound, transform.position);
+				}
+
+
+
+
+			}
+		
+
+			if (other.gameObject.CompareTag("Bullet")){
 
 				Destroy(this.gameObject);
-			}
-			else
-			{
-				AudioSource.PlayClipAtPoint(reboundSound, transform.position);
-			}
-
-
-
-
-		}
-
-		if (other.gameObject.CompareTag("Bullet")){
-
-			Destroy(this.gameObject);
-			Shotbullet.bulletcount -= 1;
+				Shotbullet.bulletcount -= 1;
 			
+			}
 		}
 
 
