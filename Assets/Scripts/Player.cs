2892aa1b8715a807d6e176c985b3a7b965ed7b19
服部壮一郎 <clover_2468@yahoo.co.jp>
2019-06-01 13:48:21 +0900

    現在

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,7 +17,40 @@ public class Player : MonoBehaviour
 		joystick = GameObject.Find("Joystick").GetComponent<Joystick>();
 		onFireButton = GameObject.Find("OnFireButton").GetComponent<Button>();
 		onFireButton.onClick.AddListener(() => shotBullet.ButtonShot());
+		
 	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+
+
+		if (stream.isWriting)
+		{
+			Rigidbody rigidbody = this.gameObject.GetComponent<Rigidbody>();
+			stream.SendNext(rigidbody.position);
+       		stream.SendNext(rigidbody.rotation);
+			stream.SendNext(rigidbody.velocity);
+		}
+		else
+		{
+			networkPosition = (Vector3)stream.ReceiveNext();
+       		networkRotation = (Quaternion)stream.ReceiveNext();
+			rigidbody.velocity = (Vector3)stream.ReceiveNext();
+
+
+       		float lag = Mathf.Abs((float)(PhotonNetwork.time - info.timestamp));
+       		networkPosition += (this.velocity * lag);
+		}		
+	}
+
+	public void FixedUpdate()
+	{
+		if (photonView.isMine){
+			rigidbody.position = Vector3.MoveTowards(this.position, networkPosition, Time.fixedDeltaTime);
+			rigidbody.rotation = Quaternion.RotateTowards(rigidbody.rotation, networkRotation, Time.fixedDeltaTime * 100.0f);
+		}
+	}
+
+
 
 
 	void Update()
